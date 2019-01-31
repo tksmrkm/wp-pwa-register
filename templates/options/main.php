b215a7bfd65964b782440701e1d0e66829e761ac
tksmrkm <nis.murakami@gmail.com>
2019-01-31 23:48:24 +0900

    Add interval

--- templates/options/main.php
+++ templates/options/main.php
@@ -1,7 +1,11 @@
 <div class="wrap">
     <h2>Choose WebPushTarget</h2>
-    <button id="button">Get DATA</button>
+    <div>
+        <p>インターバル：<span id="rangeValue">1000</span>ms</p>
+        <input type="range" min="0" max="10000" value="1000" step="100" id="range">
+    </div>
     <textarea id="filtered" style="width: 100%;"></textarea>
+    <button id="button">Get DATA</button>
     <div id="loader" style="position: fixed; left: 0; top: 0; width: 100%; height: 100%; z-index: 100; background: rgba(0, 0, 0, 0.5); display: none;"></div>
 </div>
 
@@ -10,15 +14,19 @@
 const textarea = document.getElementById('filtered'),
 loader = document.getElementById('loader'),
 button = document.getElementById('button'),
+range = document.getElementById('range'),
+rangeValue = document.getElementById('rangeValue'),
 getIds = (page = 1, limit = 1000, data = []) => {
     return fetch(`/wp-json/wp_pwa_register/v1/notification_ids?page=${page}&limit=${limit}`)
     .then(response => response.json())
     .then(json => {
         if (json.data.length) {
-            return getIds(json.meta.page + 1, json.meta.limit, [
-                ...data,
-                ...json.data
-            ])
+            return setTimeout(() => {
+                getIds(json.meta.page + 1, json.meta.limit, [
+                    ...data,
+                    ...json.data
+                ])
+            }, Number(range.value))
         }
         loader.style.display = "none"
         textarea.textContent = JSON.stringify(data)
@@ -31,5 +39,9 @@ getIds = (page = 1, limit = 1000, data = []) => {
 button.addEventListener('click', () => {
     loader.style.display = "block"
     getIds()
+})
+
+range.addEventListener('input', e => {
+    rangeValue.textContent = e.target.value
 })
 </script>
