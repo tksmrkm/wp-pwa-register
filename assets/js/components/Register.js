26ee9ea9568bf73d59762bb2278b252abdab3060
tksmrkm <nis.murakami@gmail.com>
2019-02-05 15:45:02 +0900

    update register script

--- assets/js/components/Register.js
+++ assets/js/components/Register.js
@@ -6,11 +6,19 @@ export default class Register
  * this.auth      firebaseのauth()オブジェクト
  */
     constructor(logger) {
-        // console.count('constructor');
-        this.logger = logger;
-        this.messaging = firebase.messaging();
-        this.auth = firebase.auth();
-        this.refresh = false;
+        // console.count('constructor')
+        this.logger = logger
+        this.messaging = firebase.messaging()
+        this.auth = firebase.auth()
+        this.refresh = false
+
+        // bind functions
+        this.refreshToken = this.refreshToken.bind(this)
+        this.onAuthStateChanged = this.onAuthStateChanged.bind(this)
+        this.register = this.register.bind(this)
+        this.error = this.error.bind(this)
+        this.getToken = this.getToken.bind(this)
+        this.setToken = this.setToken.bind(this)
     }
 
 /**
@@ -18,10 +26,11 @@ export default class Register
  * /pwa-service-worker.jsを登録
  */
     load() {
-        // console.count('onload');
+        // console.count('onload')
         if ('serviceWorker' in navigator) {
             navigator.serviceWorker.register('/pwa-service-worker.js')
-            .then(this.register.bind(this));
+            .then(this.register)
+            .catch(this.error)
         }
     }
 
@@ -30,16 +39,16 @@ export default class Register
  * @param  {[type]} registration
  */
     register(registration) {
-        // console.count('register');
-        this.messaging.useServiceWorker(registration);
+        // console.count('register')
+        this.messaging.useServiceWorker(registration)
 
-        const permission = this.messaging.getNotificationPermission_();
-        this.permission = permission;
+        const permission = this.messaging.getNotificationPermission_()
+        this.permission = permission
 
         if (permission === 'default') {
-            this.requestPermission();
+            this.requestPermission()
         } else if (permission === 'granted') {
-            this.messaging.onTokenRefresh(this.refreshToken.bind(this));
+            this.messaging.onTokenRefresh(this.refreshToken)
         }
     }
 
@@ -48,33 +57,33 @@ export default class Register
  * @return {[type]} [description]
  */
     requestPermission() {
-        // console.count('requestPermission');
+        // console.count('requestPermission')
         this.messaging.requestPermission()
-        .then(this.getToken.bind(this))
-        .catch(this.error.bind(this));
+        .then(this.getToken)
+        .catch(this.error)
     }
 
     refreshToken() {
-        // console.count('RefreshToken');
-        this.refresh = true;
-        this.logger.logging({msg: 'will refresh token'});
-        return this.getToken();
+        // console.count('RefreshToken')
+        this.refresh = true
+        this.logger.logging({msg: 'will refresh token'})
+        return this.getToken()
     }
 
     getToken() {
         // console.count('getToken')
         return this.messaging.getToken()
-        .then(this.setToken.bind(this))
-        .catch(this.error.bind(this));
+        .then(this.setToken)
+        .catch(this.error)
     }
 
     setToken(token) {
-        // console.count('setToken');
-        // console.log(token);
-        this.token = token;
-        this.auth.onAuthStateChanged(this.onAuthStateChanged.bind(this));
+        // console.count('setToken')
+        // console.log(token)
+        this.token = token
+        this.auth.onAuthStateChanged(this.onAuthStateChanged)
         this.auth.signInAnonymously()
-        .catch(this.error.bind(this));
+        .catch(this.error)
     }
 
 /**
@@ -83,11 +92,11 @@ export default class Register
  * @param  {[type]} user [description]
  */
     onAuthStateChanged(user) {
-        // console.count('onAuthStateChanged');
+        // console.count('onAuthStateChanged')
         if (user) {
-            // console.log(user);
-            this.uid = user.uid;
-            this.findUser();
+            // console.log(user)
+            this.uid = user.uid
+            this.findUser()
         }
     }
 
@@ -97,24 +106,30 @@ export default class Register
  * @return {[type]} [description]
  */
     findUser() {
-        // console.count('findUser');
+        // console.count('findUser')
         const headers = new Headers({
             Authorization: 'Basic ' + WP_REGISTER_SERVICE_WORKER.base64
-        });
+        })
 
-        fetch(`${WP_REGISTER_SERVICE_WORKER.root}wp/v2/pwa_users?status=draft&search=${this.uid}`, {
-            headers: headers
+        fetch(`${WP_REGISTER_SERVICE_WORKER.root}wp/v2/pwa_users?search=${this.uid}&status=draft`, {
+            headers: headers,
+            credentials: 'omit'
         })
         .then(response => {
-            return response.json()
+            if (response.ok) {
+                return response.json()
+            }
+
+            throw `${response.status}: ${response.statusText}`
         })
         .then(json => {
-            let id = null;
+            let id = null
             if (json.length) {
-                id = json.pop().id;
+                id = json.pop().id
             }
-            this.saveUser(id);
+            this.saveUser(id)
         })
+        .catch(this.error)
     }
 
 /**
@@ -125,43 +140,47 @@ export default class Register
  */
     saveUser(pwa_user_id) {
         if (WP_REGISTER_SERVICE_WORKER.debug && this.permission === 'granted') {
-            const fetchBody = new FormData();
-            fetchBody.append('permission', 'granted');
-            fetchBody.append('uid', this.uid);
-            fetchBody.append('pwa_user_id', pwa_user_id);
-            fetchBody.append('token', this.token);
-            this.logger.logging(fetchBody);
+            const fetchBody = new FormData()
+            fetchBody.append('permission', 'granted')
+            fetchBody.append('uid', this.uid)
+            fetchBody.append('pwa_user_id', pwa_user_id)
+            fetchBody.append('token', this.token)
+            this.logger.logging(fetchBody)
         }
 
         if (this.refresh) {
-            const ref = new FormData();
-            ref.append('msg', 'RefreshData');
-            ref.append('uid', this.uid);
-            ref.append('pwa_user_id', pwa_user_id);
-            ref.append('token', this.token);
-            this.logger.logging(ref);
+            const ref = new FormData()
+            ref.append('msg', 'RefreshData')
+            ref.append('uid', this.uid)
+            ref.append('pwa_user_id', pwa_user_id)
+            ref.append('token', this.token)
+            this.logger.logging(ref)
         }
-        // console.count('saveUser');
-        // console.log(pwa_user_id);
+        // console.count('saveUser')
+        // console.log(pwa_user_id)
         const headers = new Headers({
-            Authorization: 'Basic ' + WP_REGISTER_SERVICE_WORKER.base64
-        });
-        const data = new FormData();
-        let entrypoint = `${WP_REGISTER_SERVICE_WORKER.root}wp/v2/pwa_users`;
-        if (pwa_user_id) {
-            entrypoint += '/' + pwa_user_id;
+            "Authorization": 'Basic ' + WP_REGISTER_SERVICE_WORKER.base64,
+            "Content-Type": "application/json"
+        })
+        const data = {
+            title: this.uid,
+            token: this.token
         }
-        data.set('title', this.uid);
-        data.set('token', this.token);
+        let entrypoint = `${WP_REGISTER_SERVICE_WORKER.root}wp/v2/pwa_users`
+        if (pwa_user_id) {
+            entrypoint += '/' + pwa_user_id
+        }
 
         fetch(entrypoint, {
             method: 'POST',
             headers: headers,
-            body: data
-        });
+            body: JSON.stringify(data),
+            credentials: 'omit'
+        })
+        .catch(this.error)
     }
 
     error(err) {
-        console.warn(err);
+        console.warn(err)
     }
 }
