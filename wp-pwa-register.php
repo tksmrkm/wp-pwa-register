b150a623ea004c43d07734b5fd86da61c5c29f26
tksmrkm <murakami@nis-group.net>
2023-08-09 20:59:57 +0900

    v1.5.1

--- wp-pwa-register.php
+++ wp-pwa-register.php
@@ -11,7 +11,7 @@ define('VERSION', $pkg->version);
 /*
 Plugin Name: WP PWA Register
 Description: WordpressにPWA(Progressive Web Apps)を適用させる。
-Version: 1.5.0
+Version: 1.5.1
 */
 
 require_once ROOT . DS . 'vendor' . DS . 'autoload.php';
