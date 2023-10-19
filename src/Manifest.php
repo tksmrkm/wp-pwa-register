62f2c44190bcfb580930dba4d59a8cd7386e122b
tksmrkm <nis.murakami@gmail.com>
2023-10-19 19:52:37 +0900

    Update: rewrite rules implements on parse_request action instead of template_redirect action

--- src/Manifest.php
+++ src/Manifest.php
@@ -4,34 +4,45 @@ namespace WpPwaRegister;
 
 class Manifest
 {
-    use traits\Singleton;
-    
-    private function init($container)
+    const QUERY_ROUTE_KEY = 'pwa-manifest';
+
+    private Customizer $customizer;
+
+    public function __construct(Customizer $customizer)
     {
-        $this->customizer = $container['customizer'];
+        $this->customizer = $customizer;
+
         add_filter('query_vars', [$this, 'addVar']);
-        add_action('template_redirect', [$this, 'redirect']);
+        add_action('init', [$this, 'registerRoute']);
+        add_action('parse_request', [$this, 'parseRequest']);
     }
 
+    public function registerRoute()
+    {
+        add_rewrite_rule('^pwa-manifest.json/?$', 'index.php?' . self::QUERY_ROUTE_KEY . '=1', 'top');
+    }
+    
     public function addVar($vars)
     {
-        $vars[] = 'manifest';
+        $vars[] = self::QUERY_ROUTE_KEY;
         return $vars;
     }
 
-    public function redirect()
+    public function parseRequest($wp)
     {
-        if ($manifest = get_query_var('manifest')) {
-            header('Content-Type: application/json; charset=UTF-8');
-            $name = $this->customizer->get_theme_mod('name', get_bloginfo('name'));
-            $shortName = $this->customizer->get_theme_mod('short_name') ?: null;
-            $display = $this->customizer->get_theme_mod('display', 'standalone');
-            $startUrl = $this->customizer->get_theme_mod('start-url', '/');
-            $themeColor = $this->customizer->get_theme_mod('theme-color', '#333');
-            $backgroundColor = $this->customizer->get_theme_mod('background-color', '#FFF');
-            $icon = $this->getIcon();
-            include_once ROOT . DS . 'templates' . DS . 'manifest.json';
-            exit;
+        if (isset($wp->query_vars[self::QUERY_ROUTE_KEY])) {
+            if ($wp->query_vars[self::QUERY_ROUTE_KEY] === '1') {
+                header('Content-Type: application/json; charset=UTF-8');
+                $name = $this->customizer->get_theme_mod('name', get_bloginfo('name'));
+                $shortName = $this->customizer->get_theme_mod('short_name') ?: null;
+                $display = $this->customizer->get_theme_mod('display', 'standalone');
+                $startUrl = $this->customizer->get_theme_mod('start-url', '/');
+                $themeColor = $this->customizer->get_theme_mod('theme-color', '#333');
+                $backgroundColor = $this->customizer->get_theme_mod('background-color', '#FFF');
+                $icon = $this->getIcon();
+                include_once ROOT . DS . 'templates' . DS . 'manifest.json';
+                exit;
+            }
         }
     }
 
