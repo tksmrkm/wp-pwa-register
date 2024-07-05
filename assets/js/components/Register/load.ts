ad016f468ca033a5154add1736c7502372e298e3
tksmrkm <murakami@nis-group.net>
2024-07-06 01:40:53 +0900

    Update: register
    update token once per month
    use HTTP v1 subscribe endpoint

--- assets/js/components/Register/load.ts
+++ assets/js/components/Register/load.ts
@@ -3,12 +3,36 @@ import handleShow from './show';
 import handleRequest from './request';
 import { handleRegisterSuccess } from './register';
 
+export const expiredKey = 'tokenSubscribedDatetime'
+
 const handleLoad = async () => {
     const supported = await isSupported()
 
     if (!supported) {
         // not support this browser
         return false
+    }
+
+    const { permission } = Notification
+
+    if (permission === 'default') {
+        localStorage.removeItem(expiredKey)
+    }
+
+    const expired = localStorage.getItem(expiredKey)
+    if (expired) {
+        const now = new Date()
+        const diff = 30 * 24 * 60 * 60 * 1000 // 30days
+
+        if (Number(expired) + diff > now.getTime()) {
+            /**
+             * トークンの更新は30日に一回
+             * 
+             * expiredがlocalStorageに記録されており
+             * 30日経過していない場合はgetTokenまで進まない
+             */
+            return false
+        }
     }
 
     if (!WP_REGISTER_SERVICE_WORKER.register.useDialog) {
@@ -20,7 +44,7 @@ const handleLoad = async () => {
         return false
     }
 
-    if (Notification.permission === 'granted') {
+    if (permission === 'granted') {
         // granted browser renew tokens
         const registration = await navigator.serviceWorker.getRegistration()
         if (registration) {
@@ -29,7 +53,7 @@ const handleLoad = async () => {
         return false
     }
 
-    if (Notification.permission === 'denied') {
+    if (permission === 'denied') {
         // denied browser do nothing
         // we can show banners for re-subscribing
         return false
--- assets/js/components/Register/register.ts
+++ assets/js/components/Register/register.ts
@@ -1,6 +1,7 @@
 import { getToken, getMessaging } from 'firebase/messaging'
 import { signInAnonymously, getAuth } from 'firebase/auth'
 import app from '~/utils/firebase'
+import { expiredKey } from './load'
 
 type UserProps = {
     id: string;
@@ -27,67 +28,23 @@ export const handleRegisterSuccess = async (serviceWorkerRegistration: ServiceWo
         throw new Error('can not find user')
     }
 
-    // find user
-    const headers = new Headers({
-        "Authorization": `Basic ${WP_REGISTER_SERVICE_WORKER.base64}`
+    // user.user.uid
+    // token
+    const subscribeBody = new FormData()
+    subscribeBody.append('uid', user.user.uid)
+    subscribeBody.append('token', token)
+    const result = await fetch('/pwa-subscribe', {
+        method: 'POST',
+        body: subscribeBody
     })
-
-    const query = new URLSearchParams({
-        search: user.user.uid,
-        status: 'draft'
-    })
-
-    const fetchedUser = await fetch(`${WP_REGISTER_SERVICE_WORKER.root}wp/v2/pwa_users?${query}`, {
-        headers,
-        credentials: 'omit'
-    })
-    .then(response => {
-        if (response.ok) {
-            return response.json()
+    .then(res => {
+        if (res.ok) {
+            return res.json()
         }
 
-        throw `${response.status}: ${response.statusText}`
-    })
-    .then((json: UserProps[]) => {
-        const user = json.length ? json.pop(): null
-        return user
-    })
-    .catch(console.warn)
-
-    // save user
-    const saveHeaders = new Headers({
-        Authorization: `Basic ${WP_REGISTER_SERVICE_WORKER.base64}`,
-        "Content-Type": "application/json"
+        throw new Error(res.statusText)
     })
 
-    const body = JSON.stringify({
-        title: user.user.uid,
-        token
-    })
-
-    const entry = [`${WP_REGISTER_SERVICE_WORKER.root}wp/v2/pwa_users`]
-
-    if (fetchedUser) {
-        if (fetchedUser.token === token && !fetchedUser.deleted) {
-            // tokenの更新がないのでアップデート処理はキャンセル
-            return
-        }
-
-        entry.push(fetchedUser.id)
-    }
-
-    fetch(entry.join('/'), {
-        method: 'post',
-        headers: saveHeaders,
-        body,
-        credentials: 'omit'
-    })
-    .then(response => {
-        if (response.ok) {
-            return response.json()
-        }
-    })
-    .then(json => {
-        return json
-    })
+    console.log(result)
+    localStorage.setItem(expiredKey, (new Date()).getTime().toString())
 }
