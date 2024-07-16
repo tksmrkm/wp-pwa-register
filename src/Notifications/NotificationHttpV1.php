2c77ef8bc85355a8d54c1d2ddb814f2e55d01c4b
tksmrkm <nis.murakami@gmail.com>
2024-07-16 18:21:08 +0900

    Fix: send data as string

--- src/Notifications/NotificationHttpV1.php
+++ src/Notifications/NotificationHttpV1.php
@@ -53,10 +53,10 @@ class NotificationHttpV1
                     'body' => $content
                 ],
                 'data' => [
-                    'version' => 2,
+                    'version' => "2",
                     'icon' => $icon,
                     'link' => $link,
-                    'post_id' => $post_id
+                    'post_id' => "$post_id"
                 ]
             ]
         ];
